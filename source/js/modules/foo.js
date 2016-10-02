3e146025cfcb38301a4ed8c0fe95a55cf2594d20
Simon Walsh <simon@walsh.si>
2016-10-02 18:14:32 -0400

    First commit, porting over what we had started for GHJ. Likely will be deleted from history at some point!

---
+++ source/js/modules/foo.js
@@ -0,0 +1,23 @@
+//
+//
+// Foo
+//
+//
+const Foo = {
+
+  //
+  // Globals
+  //
+  bar: 'test',
+
+  //
+  // Methods
+  //
+  log: () => { return console.log('HEY!') }
+
+}
+
+//
+// Export
+//
+export default Foo
---
+++ source/js/theme.js
@@ -0,0 +1,25 @@
+//
+//
+// Theme
+//
+// NOTE: this is the global entry point
+// for the app
+//
+//
+
+//
+// Requires
+//
+require('lodash')
+
+//
+// Imports
+//
+import Foo from './modules/foo'
+
+//
+// jQuery
+//
+$(function() {
+  Foo.log()
+})
