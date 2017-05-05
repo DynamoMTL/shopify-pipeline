341a4f28775a9a1c9f6be9cf0624a0f604531537
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-05-05 15:34:04 -0400

    Load shopify.yml into JS config

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const YAML = require('yamljs');
+
 module.exports = {
   regex: {
     images: /\.(png|svg|jpg|gif)$/,
     static: /\.(liquid|json)$/,
   },
+
+  shopify: YAML.load(path.join(__dirname, 'shopify.yml')),
 };
