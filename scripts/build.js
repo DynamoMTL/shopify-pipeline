9b446ea879358a70c84410d5503c55a492c64c09
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-05-11 12:02:24 -0400

    Allow passing in the --env argument to scripts

--- scripts/build.js
+++ scripts/build.js
@@ -4,15 +4,17 @@
 const argv = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 const webpack = require('webpack');
-const config = require('../config/webpack.prod.conf');
+const webpackConfig = require('../config/webpack.prod.conf');
+const config = require('../config');
 const shopify = require('../lib/shopify-deploy');
+const env = require('../lib/getShopifyEnvOrDie.js')(argv.env, config.shopify);
 
 // // Copy over everything from the 'static' folder
 // fs.copySync(path.join(__dirname, '../static'), path.join(__dirname, '../dist'), {
 //   dereference: true,
 // });
 
-webpack(config, (err, stats) => {
+webpack(webpackConfig, (err, stats) => {
   if (err) throw err;
 
   process.stdout.write(`${stats.toString({
@@ -24,7 +26,7 @@ webpack(config, (err, stats) => {
   })}`);
 
   if (argv.deploy) {
-    shopify.overwrite().then(() => {
+    shopify.overwrite(env).then(() => {
       console.log(chalk.green('\nFiles overwritten successfully!\n'));
     }).catch((error) => {
       console.log(`\n${chalk.red(error)}\n`);
