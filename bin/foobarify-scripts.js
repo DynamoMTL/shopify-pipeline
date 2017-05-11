b09e04ef83ae6626b66e7756425f58010ce8acd0
Simon Walsh <simon@walsh.si>
2017-05-11 14:25:28 -0400

    Adding ava support, removing unused scripts in the tool

--- bin/foobarify-scripts.js
+++ bin/foobarify-scripts.js
@@ -1,23 +1,32 @@
 #!/usr/bin/env node
 const spawn = require('cross-spawn');
+const paths = require('../config/paths');
 
 const script = process.argv[2];
 const args = process.argv.slice(3);
 
+let result;
+
 switch (script) {
   case 'build':
   case 'serve':
-  case 'test':
-    const result = spawn.sync(
+    result = spawn.sync(
       'node',
       [require.resolve(`../scripts/${script}`)].concat(args),
       { stdio: 'inherit' }
     );
     process.exit(result.status);
     break;
-
+  case 'test':
+    result = spawn.sync(
+      'ava',
+      [`${paths.src}/specs/**/*.js`].concat(args),
+      { stdio: 'inherit' }
+    );
+    process.exit(result.status);
+    break;
   default:
-    console.log(`Unknown script "${script }".`);
+    console.log(`Unknown script "${script}".`);
     console.log('Perhaps you need to update foobarify-scripts?');
     break;
 }
