0e1daffd87618038c507ec4e69e06ff3cd2c1eff
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-06-09 13:24:19 -0400

    Export a function out of init.js and accept the root directory as it's only argument

--- scripts/init.js
+++ scripts/init.js
@@ -1,24 +1,25 @@
 const fs = require('fs-extra')
 const path = require('path')
-const paths = require('../config/paths')
 
 // @TODO find out a way to never run this twice ?
 
-const packageJsonPath = path.join(paths.root, 'package.json')
-// eslint-disable-next-line
-const packageJson = require(packageJsonPath)
+module.exports = (root) => {
+  const packageJsonPath = path.join(root, 'package.json')
+  // eslint-disable-next-line
+  const packageJson = require(packageJsonPath)
 
-packageJson.scripts = {
-  serve: 'foobarify-scripts serve',
-  build: 'foobarify-scripts build',
-  deploy: 'foobarify-scripts build --deploy',
-  test: 'foobarify-scripts test --coverage'
+  packageJson.scripts = {
+    serve: 'foobarify-scripts serve',
+    build: 'foobarify-scripts build',
+    deploy: 'foobarify-scripts build --deploy',
+    test: 'foobarify-scripts test --coverage'
+  }
+  packageJson.babel = { presets: ['env'] }
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+
+  fs.copySync(path.join(__dirname, '../template'), root)
+  fs.copySync(path.join(__dirname, '../README.md'), path.join(root, 'README.md'))
+
+  fs.moveSync(path.join(root, 'gitignore'), path.join(root, '.gitignore'))
 }
-packageJson.babel = { presets: ['env'] }
-
-fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
-
-fs.copySync(path.join(__dirname, '../template'), paths.root)
-fs.copySync(path.join(__dirname, '../README.md'), path.join(paths.root, 'README.md'))
-
-fs.moveSync(path.join(paths.root, 'gitignore'), path.join(paths.root, '.gitignore'))
