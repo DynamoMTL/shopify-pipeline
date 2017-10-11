70e0964bd5593f727c725ed8c3ed926a8e4ac176
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-10-11 11:25:37 -0400

    Allow all values for CORS on dev server, works with custom Shopify domains

--- scripts/serve.js
+++ scripts/serve.js
@@ -71,7 +71,7 @@ function getFilesFromAssets(assets) {
 }
 
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', shopifyUrl)
+  res.set('Access-Control-Allow-Origin', '*')
   next()
 })
 
